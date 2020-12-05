e908b85bdb70bf3ff508eac779420a2854afd19c
Per von Zweigbergk <pvz@pvz.pp.se>
2020-12-05 13:32:21 +0100

    Simplify day 5 solution.

--- Day5-Solution.ps1
+++ Day5-Solution.ps1
@@ -1,37 +1,20 @@
-﻿function DecodeBinary($String, $Zero, $One) {
-    [convert]::ToInt32($String.Replace($Zero, '0').Replace($One, '1'), 2)
-}
-
-function Get-BoardingPass {
+﻿function Get-SeatID {
     Get-Content $PSScriptRoot\Day5-Input.txt | % {
-        if ($_ -match "^([FB]{7})([LR]{3})$") {
-            $Row = DecodeBinary $matches[1] 'F' 'B'
-            $Column = DecodeBinary $matches[2] 'L' 'R'
-            $SeatID = (8 * $Row) + $Column
-
-            [pscustomobject]@{
-                Code   = $_
-                Row    = $Row
-                Column = $Column
-                SeatID = $SeatID
-            }
-        } else {
-            throw "Unparsable code: $_"
-        }
+        [convert]::ToInt32(($_ -replace '[FL]', '0' -replace '[BR]', '1'), 2)
     }
 }
 
-$BoardingPasses = Get-BoardingPass
+$SeatIDs = Get-SeatID
 
 # Find the range we should be looking in by sorting the boarding passes by seat ID.
 
-$MeasureSeatID = $BoardingPasses | Measure-Object -Property SeatID -Minimum -Maximum # BEATS PER MINUTE!
+$MeasureSeatID = $SeatIDs | Measure-Object -Minimum -Maximum # BEATS PER MINUTE!
 $MinSeatID = $MeasureSeatID.Minimum
 $MaxSeatID = $MeasureSeatID.Maximum
 
 # Put all the SeatIDs in a HashSet so we can easilly see if a seat is taken or not
 
-$SeatIDSet = [System.Collections.Generic.HashSet[int]]($BoardingPasses.SeatID)
+$SeatIDSet = [System.Collections.Generic.HashSet[int]]($SeatIDs)
 
 # Find the SeatID that's not taken. (Also we know it's not MinSeatID and MaxSeatID because they exist. So...)
 
